e4af352431bc009c16bd108e1151727c38c198f8
Josue <josuji.alfa@gmail.com>
2020-03-27 16:35:02 -0600

    make it better

--- clientvpn.py
+++ clientvpn.py
@@ -6,7 +6,47 @@ import cvpn_mail
 import datetime
 
 from flask import Flask
-from apscheduler.schedulers.blocking import BlockingScheduler
+# from apscheduler.schedulers.blocking import BlockingScheduler
+
+import traceback
+from werkzeug.wsgi import ClosingIterator
+
+class AfterResponse:
+    def __init__(self, app=None):
+        self.callbacks = []
+        if app:
+            self.init_app(app)
+
+    def __call__(self, callback):
+        self.callbacks.append(callback)
+        return callback
+
+    def init_app(self, app):
+        # install extension
+        app.after_response = self
+
+        # install middleware
+        app.wsgi_app = AfterResponseMiddleware(app.wsgi_app, self)
+
+    def flush(self):
+        for fn in self.callbacks:
+            try:
+                fn()
+            except Exception:
+                traceback.print_exc()
+
+class AfterResponseMiddleware:
+    def __init__(self, application, after_response_ext):
+        self.application = application
+        self.after_response_ext = after_response_ext
+
+    def __call__(self, environ, after_response):
+        iterator = self.application(environ, after_response)
+        try:
+            return ClosingIterator(iterator, [self.after_response_ext.flush])
+        except Exception:
+            traceback.print_exc()
+            return iterator
 
 api_key = credentials.api_key
 baseurl = credentials.base_url
@@ -15,9 +55,10 @@ network_id = credentials.network_id
 client_vpn_threshold = credentials.client_vpn_threshold
 
 # initialization
-app = Flask(__name__)  # create the Flask app
+app = Flask("after_response")  # create the Flask app
+AfterResponse(app)
 
-sched = BlockingScheduler()
+# sched = BlockingScheduler()
 
 # Instantiate Meraki Python SDK Client
 dashboard = meraki.DashboardAPI(
@@ -26,49 +67,48 @@ dashboard = meraki.DashboardAPI(
         print_console=False)
 
 
-@sched.scheduled_job('interval', seconds=20)
+@app.after_response
 def timed_nertwork():
-        # i = 0
+    # i = 0
+    # while True:
         clientvpn = dashboard.clients.getNetworkClients(networkId=network_id)
         cvpn_users = 0
         for item in clientvpn:
-                # Check if Client Device belongs to ClientVPN Subnet
-                l = re.split('(.*)\.(.*)\.(.*)\.(.*)', item['ip'])
-                network_add = l[1:-1]
-                if network_add[0:3] == ['192', '168', '92'] or network_add[0:3] == ['192', '168', '93']:
-                        curr_stamp = datetime.datetime.utcnow().timestamp()
-                        dt = datetime.datetime.strptime(item['lastSeen'], '%Y-%m-%dT%H:%M:%SZ')
-                        client_stamp = dt.timestamp()
-                        # If Client VPN user has been seen in the last 10 minutes, count it
-                        if (curr_stamp - 600) <= client_stamp:
-                                cvpn_users = cvpn_users + 1
+            # Check if Client Device belongs to ClientVPN Subnet
+            l = re.split('(.*)\.(.*)\.(.*)\.(.*)', item['ip'])
+            network_add = l[1:-1]
+            if network_add[0:3] == ['192', '168', '92'] or network_add[0:3] == ['192', '168', '93']:
+                curr_stamp = datetime.datetime.utcnow().timestamp()
+                dt = datetime.datetime.strptime(item['lastSeen'], '%Y-%m-%dT%H:%M:%SZ')
+                client_stamp = dt.timestamp()
+                # If Client VPN user has been seen in the last 10 minutes, count it
+                if (curr_stamp - 600) <= client_stamp:
+                    cvpn_users = cvpn_users + 1
         print(cvpn_users)
         # Set client threshold to desired amount in credentials file
         if cvpn_users >= client_vpn_threshold:
-                sender_email = credentials.email
-                password = credentials.password
-                subject = "WARNING TOO MANY VPN USERS!"
-                body = "ALERT, TOTAL CLIENT VPN USERS IS {} ".format(cvpn_users)
-                body = body + "\nREMEMBER TO START THE SCRIPT AGAIN!"
-                cvpn_mail.send_mail(sender_email, password, subject, body)
-                # i = 1
+            sender_email = credentials.email
+            password = credentials.password
+            subject = "WARNING TOO MANY VPN USERS!"
+            body = "ALERT, TOTAL CLIENT VPN USERS IS {} ".format(cvpn_users)
+            body = body + "\nREMEMBER TO START THE SCRIPT AGAIN!"
+            cvpn_mail.send_mail(sender_email, password, subject, body)
+            # i = 1
         else:
-                now = datetime.datetime.today()
-                dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+            now = datetime.datetime.today()
+            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 
-                sender_email = credentials.email
-                password = credentials.password
-                subject = "WARNING TOO MANY VPN USERS!"
-                body = "ALERT, TOTAL CLIENT VPN USERS IS {} ".format(cvpn_users)
-                body = body + "\nREMEMBER TO START THE SCRIPT AGAIN! " + dt_string
-                cvpn_mail.send_mail(sender_email, password, subject, body)
-
-        return "Hola"
+            sender_email = credentials.email
+            password = credentials.password
+            subject = "WARNING TOO MANY VPN USERS!"
+            body = "ALERT, TOTAL CLIENT VPN USERS IS {} ".format(cvpn_users)
+            body = body + "\nREMEMBER TO START THE SCRIPT AGAIN! " + dt_string
+            cvpn_mail.send_mail(sender_email, password, subject, body)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def main():
-        return sched.start()
+        return "Success!\n"
 
 
 if __name__ == '__main__':
